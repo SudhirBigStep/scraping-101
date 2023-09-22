fb0e371ac19aacba0c56f82addc59f23443c2423
SudhirBigStep <sudhir.taneja@bigsteptech.com>
2023-09-22 21:02:44 +0530

    initial commit

---
+++ loginIntoGChat.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+
+async function loginToGoogleChat() {
+    const browser = await puppeteer.launch({ headless: false})
+  const page = await browser.newPage()
+
+  const navigationPromise = page.waitForNavigation()
+
+  await page.goto('https://accounts.google.com/')
+
+  await navigationPromise
+
+  await page.waitForSelector('input[type="email"]')
+  await page.click('input[type="email"]')
+
+  await navigationPromise
+
+  //TODO : change to your email 
+  await page.type('input[type="email"]', 'sudhir.taneja@bigsteptech.com')
+
+  await page.waitForSelector('#identifierNext')
+  await page.click('#identifierNext')
+
+  await page.waitFor(500);
+
+  await page.waitForSelector('input[type="password"]')
+  await page.click('input[type="email"]')
+  await page.waitFor(500);
+
+  //TODO : change to your password
+  await page.type('input[type="password"]', 'yourpassword')
+
+  await page.waitForSelector('#passwordNext')
+  await page.click('#passwordNext')
+
+  await navigationPromise
+
+  //await browser.close()
+  await browser.close();
+}
+
+module.exports = {
+    loginToGoogleChat
+}
---
+++ scrapper.js
@@ -0,0 +1,84 @@
+const puppeteer = require('puppeteer');
+const { loginToGoogleChat } = require('./loginIntoGChat');
+
+async function scrapeGoogleChat() {
+  const browser = await puppeteer.launch({ headless: false, defaultViewport: null, executablePath: '/opt/google/chrome/chrome'})
+  const page = await browser.newPage()
+
+  const navigationPromise = page.waitForNavigation()
+
+  await page.goto('https://chat.google.com/')
+
+  await navigationPromise
+
+  await page.waitForSelector('gws-button[label="Sign in to Google Workspace to access Chat"]')
+  await page.click('gws-button[label="Sign in to Google Workspace to access Chat"]')
+
+  await navigationPromise
+
+  //TODO : change to your email 
+  await page.waitForSelector('input[type="email"]');
+  await page.click('input[type="email"]')
+  await page.type('input[type="email"]', 'sudhir.taneja@bigsteptech.com')
+  
+
+  await page.waitForSelector('#identifierNext')
+  await page.click('#identifierNext')
+  await page.waitForTimeout(3000);
+
+  await page.waitForSelector('input[type="password"]')
+  await page.click('input[type="password"]')
+
+  //TODO : change to your password
+  await page.type('input[type="password"]', 'IknowThat@11')
+
+  await page.waitForSelector('#passwordNext')
+  await page.click('#passwordNext')  
+  // await page.waitForSelector('div[data-challengeid="3"]');
+  // await page.click('div[data-challengeid="3"]')
+
+  // await page.waitForSelector('input[type="tel"]')
+  // await page.click('input[type="tel"]')
+
+  // //TODO : change to your password
+  // await page.type('input[type="tel"]', '')
+  await page.waitForTimeout(40000);
+  page.goto('https://mail.google.com/chat/u/0/#chat/space/AAAAKLD2ppk')
+  await page.waitForNavigation();
+
+    // Extract chat messages
+    const messages = await page.evaluate(() => {
+      const messageElements = Array.from(document.querySelectorAll('div.message'));
+      return messageElements.map((element) => {
+        const sender = element.querySelector('div.message-sender').textContent;
+        const timestamp = element.querySelector('div.message-timestamp').textContent;
+        const content = element.querySelector('div.message-content').textContent;
+        return { sender, timestamp, content };
+      });
+    });
+
+    const newsletterHTML = generateNewsletterHTML(messages);
+
+  //await browser.close()
+  // await browser.close();
+}
+
+
+function generateNewsletterHTML(messages) {
+  // Generate HTML code for the newsletter
+  // You can use a template library like Handlebars.js or build the HTML manually
+
+  let newsletterHTML = '<html><head><title>Google Chat Newsletter</title></head><body>';
+
+  messages.forEach((message) => {
+    newsletterHTML += `<p><strong>${message.sender}</strong> - ${message.timestamp}</p>`;
+    newsletterHTML += `<p>${message.content}</p>`;
+  });
+
+  newsletterHTML += '</body></html>';
+
+  return newsletterHTML;
+}
+
+scrapeGoogleChat();
+
